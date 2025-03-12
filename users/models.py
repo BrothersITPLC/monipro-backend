6bdc82674e1cda96293248958f302417f50ef50e
aman7ph <aman7ph@gmail.com>
2025-03-13 00:50:33 +0300

    working on user managment

--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, password2=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+        if password != password2:
+            raise ValueError("Passwords don't match")
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        user = self.create_user(
+            email, name, password, password2=password, **extra_fields
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="Email", max_length=255, unique=True)
