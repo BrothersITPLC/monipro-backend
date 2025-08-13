d0109306a510d46059838e260f2f4ed80b392ce8
aman-ph <amanuel.desalegn@brothersitplc.com>
2025-08-13 13:27:40 +0300

    typo fix

--- monipro/settings.py
+++ monipro/settings.py
@@ -104,7 +104,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount.providers.google",
     "allauth.socialaccount.providers.github",
     "social_django",
-    "allauth.headless"
+    "allauth.headless",
     "django_cron",
 ]
 AUTH_USER_MODEL = "users.User"
@@ -293,7 +293,7 @@ SOCIALACCOUNT_PROVIDERS = {
     "google": {
         "app": {
             "google_client_id": os.getenv("GOOGLE_CLIENT_ID", ""),
-            "google_client_secret": os.getenv("GOGLE_CLIENT_SECRET", ""),
+            "google_client_secret": os.getenv("GOOGLE_CLIENT_SECRET", ""),
         },
         "android": {
             "google_mobile_client_id": "100510992151-p9gjup17dhq1prs9clp4v8f27qsnfhbc.apps.googleusercontent.com",
